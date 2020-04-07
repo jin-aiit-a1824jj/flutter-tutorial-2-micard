8a3916b14a2456771d0a88535dd72e8ba00036ed
a1824jj <a1824jj@aiit.ac.jp>
2020-04-07 15:28:12 +0900

    add material icon & use icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,50 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0
+                    ),
+                    Text('+44 123 456 789',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SansPro',
+                      color: Colors.teal.shade900
+                    ))
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                        width: 10.0
+                    ),
+                    Text('name@email.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'SansPro',
+                            color: Colors.teal.shade900
+                        ))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
